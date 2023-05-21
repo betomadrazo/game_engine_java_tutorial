8869ad662d5398e78e94600b558a76f6fa8bf194
Alberto Madrazo <alberto.madrazo@coppel.com>
2023-05-20 21:35:27 -0600

    END OF SECTION [1:32:55] - No code here, lecture about how the GPU works. Very interesting and overwhelming.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,7 @@ import jade.Window;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("No code for this section. Only lecturing about the GPU.");
         Window window = Window.get();
         window.run();
     }
